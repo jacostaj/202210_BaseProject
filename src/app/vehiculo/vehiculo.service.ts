c063f552125f26bdadbac0f0660085ca71394527
Julian_Acosta_Jaramillo <jacostaj@uniandes.edu.co>
2022-04-29 20:55:56 -0500

    Se crea una maqueta con un diseño html inicial de la lista de vehiculo

--- src/app/vehiculo/vehiculo.service.ts
+++ src/app/vehiculo/vehiculo.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+import { Vehiculo } from './vehiculo';
 @Injectable({
   providedIn: 'root'
 })
 export class VehiculoService {
-
-constructor(private http: HttpClient) { }
-
+  private apiUrl: string = environment.baseUrl;
+  constructor(private http: HttpClient) { }
+  getVehiculos(): Observable<Vehiculo[]> {
+  return this.http.get<Vehiculo[]>(this.apiUrl);
+}
 }
