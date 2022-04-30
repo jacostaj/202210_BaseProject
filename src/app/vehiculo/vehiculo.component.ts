6cd6e5f9afee382e28efdec1078c6f92c5f9042f
Julian_Acosta_Jaramillo <jacostaj@uniandes.edu.co>
2022-04-29 20:42:19 -0500

    Confirgurar la URL del Back End

--- src/app/vehiculo/vehiculo.component.ts
+++ src/app/vehiculo/vehiculo.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Vehiculo } from './vehiculo';
 @Component({
   selector: 'app-vehiculo',
   templateUrl: './vehiculo.component.html',
   styleUrls: ['./vehiculo.component.css']
 })
 export class VehiculoComponent implements OnInit {
-
+  books: Array<Vehiculo> = [];
   constructor() { }
 
   ngOnInit() {
